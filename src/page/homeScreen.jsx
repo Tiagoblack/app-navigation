f7765f4cb29fdfc1c014c9763e8c901af622d706
tiago dos santos <xlzthiago@gmail.com>
2021-11-01 18:42:47 -0300

    mudando o header com setOptions

--- src/page/homeScreen.jsx
+++ src/page/homeScreen.jsx
@@ -9,18 +9,30 @@ export default function HomeScreen (){
     const navigation = useNavigation()
 
     const handleAboutclick = () =>{
-            navigation.navigate('about',{name});
+        navigation.setOptions({
+            title: name
+        })
+    }
+
+    const handleClick = () =>{
+        navigation.setOptions({
+            headerStyle:{
+                backgroundColor:'#000'
+            }
+        })
     }
 
     return(
         <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
             <Text>tela home</Text>
-            <Button onPress={handleAboutclick} title="Ir para about"/>
             <TextInput
                 style={{height:45, backgroundColor:'#888', width:'90%', paddingHorizontal:20, borderRadius:20}}
                 onChangeText={t=> setName(t)}
                 value={name}
             />
+            <Button onPress={handleAboutclick} title="Ir para about"/>
+            <Button onPress={handleClick} title="Ir para about"/>
+
         </View>
     );
 }
